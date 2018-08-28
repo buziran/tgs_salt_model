4c8bf35c46dc6ff560eb274eb6244d0801fd09bf
someone <someone@example.com>
2018-08-28 22:31:15 +0900

    Summarize score by coverage of GT

--- score_per_image.py
+++ score_per_image.py
@@ -25,8 +25,8 @@ flags.DEFINE_integer(
     'cv', 0, help="""index of k-fold cross validation. index must be in 0~9""")
 
 flags.DEFINE_string(
-    'score', '../output/score.csv',
-    """path to score file""")
+    'score', '../output/score',
+    """path to score directory""")
 
 FLAGS = flags.FLAGS
 
@@ -63,6 +63,11 @@ def list_image(path_input):
     return sample_ids
 
 
+def load_npz(path_pred):
+    npzfile = np.load(path_pred)
+    return npzfile['arr_0']
+
+
 def split_train_valid(sample_ids, n_splits, idx_kfold):
     kf = KFold(n_splits)
     num_samples = len(sample_ids)
@@ -78,6 +83,9 @@ def main(argv):
     sample_ids = list_image(FLAGS.input)
     train_ids, valid_ids = split_train_valid(sample_ids, N_SPLITS, FLAGS.cv)
 
+    if not os.path.isdir(FLAGS.score):
+        os.makedirs(FLAGS.score)
+
     df = pd.DataFrame(columns=["name", "score", "coverage_true", "coverage_pred"])
     df.astype({"name": str, "score": float, "coverage_true": float, "coverage_pred": float})
 
@@ -85,19 +93,41 @@ def main(argv):
 
     for train_id in tqdm(train_ids):
         y_true_path = os.path.join(FLAGS.input, "masks", train_id)
-        y_pred_path = os.path.join(FLAGS.prediction, train_id)
         y_true = np.array(Image.open(y_true_path)).astype(float)
-        y_pred = np.array(Image.open(y_pred_path)).astype(float)
         y_true = np.round(y_true / 65535.).astype(int)
-        y_pred = np.round(y_pred / 255.).astype(int)
+        y_pred_path = os.path.join(FLAGS.prediction, os.path.splitext(train_id)[0] + ".npz")
+        y_pred = load_npz(y_pred_path)
+        y_pred = np.round(y_pred).astype(int)
         score = mean_score(y_true, y_pred)
         coverage_true = np.sum(y_true) / float(ORIG_WIDTH * ORIG_HEIGHT)
         coverage_pred = np.sum(y_pred) / float(ORIG_WIDTH * ORIG_HEIGHT)
         row = {"name": train_id, "score": score, "coverage_true": coverage_true, "coverage_pred": coverage_pred}
         rows.append(row)
 
+    # Score per image
+    path_out = os.path.join(FLAGS.score, "score.csv")
     df = df.append(rows, ignore_index=True)
-    df.to_csv(FLAGS.score)
+    df.to_csv(path_out)
+
+    # Score grouped by coverage rate of ground truth
+    # bins = np.arange(0, 1.0, 0.05)
+    # ind = np.digitize(df['coverage_true'], bins)
+    # grouped = df.groupby(ind)
+
+    df = df.sort_values('coverage_true')
+    bins = np.arange(0, 1.0, 0.05)
+    bins = np.concatenate(([-1e-4], bins))
+    print(bins)
+
+    grouped = df.groupby(pd.cut(df['coverage_true'], bins))
+
+    path_out = os.path.join(FLAGS.score, "mean.csv")
+    mean_df = grouped.mean()
+    mean_df.to_csv(path_out)
+
+    path_out = os.path.join(FLAGS.score, "var.csv")
+    mean_df = grouped.var()
+    mean_df.to_csv(path_out)
 
 if __name__ == '__main__':
     app.run(main)
