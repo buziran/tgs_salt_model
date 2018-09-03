6b964c46fc9c4661c3135a08d5ae4ec2ced37756
someone <someone@example.com>
2018-09-04 00:42:58 +0900

    Add random-erasing implementation

---
+++ random_erase.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+"""
+Implementation of Random Erasing for Keras
+"""
+
+import numpy as np
+
+class RandomErasing(object):
+    def __init__(self, probability=0.5, min_size=0.02, max_size=0.4, min_aspect_ratio=0.3, max_aspect_ratio=1/0.3, min_val=0, max_val=255, pixel_wise=False, seed=None):
+        self.probability = probability
+        self.min_size = min_size
+        self.max_size = max_size
+        self.min_aspect_ratio = min_aspect_ratio
+        self.max_aspect_ratio = max_aspect_ratio
+        self.min_val = min_val
+        self.max_val = max_val
+        self.pixel_wise = pixel_wise
+        self.seed = seed
+        self.cnt = 0
+
+    def __call__(self, img):
+        if self.seed is not None:
+            np.random.seed(self.seed + self.cnt)
+
+        rank = len(img.shape)
+
+        if rank == 3:
+            height, width, channels = img.shape
+        elif rank == 2:
+            height, width = img.shape
+        else:
+            raise ValueError("image rank must be 2 or 3")
+
+        p = np.random.rand()
+
+        if p > self.probability:
+            return img
+
+        while True:
+            s = np.random.uniform(self.min_size, self.max_size) * height * width
+            r = np.random.uniform(self.min_aspect_ratio, self.max_aspect_ratio)
+            w = int(np.sqrt(s / r))
+            h = int(np.sqrt(s * r))
+            left = np.random.randint(0, width)
+            top = np.random.randint(0, height)
+
+            if left + w <= width and top + h <= height:
+                break
+
+        if self.pixel_wise:
+            if rank == 3:
+                shape = (h,w,channels)
+            elif rank == 2:
+                shape = (h,w)
+            c = np.random.uniform(self.min_val, self.max_val, shape)
+        else:
+            c = np.random.uniform(self.min_val, self.max_val)
+
+        erased_img = np.copy(img)
+        erased_img[top:top + h, left:left + w, :] = c
+
+        self.cnt += 1
+
+        return erased_img
+
+
+def main():
+    import matplotlib.pyplot as plt
+    import cv2
+    orig_im = cv2.imread('../input/train/images/000e218f21.png')
+    plt.imshow(orig_im)
+    plt.title('original')
+    plt.show()
+
+    random_erase = RandomErasing()
+    for i in range(2):
+        im = random_erase(orig_im)
+        plt.imshow(im)
+        plt.title('random erase (case-1, cnt={})'.format(i))
+        plt.show()
+
+    random_erase2 = RandomErasing(seed=11)
+    for i in range(2):
+        im = random_erase2(orig_im)
+        plt.imshow(im)
+        plt.title('random erase (case-2, cnt={})'.format(i))
+        plt.show()
+
+    random_erase3 = RandomErasing(seed=11, pixel_wise=True)
+    for i in range(2):
+        im = random_erase3(orig_im)
+        plt.imshow(im)
+        plt.title('random erase (case-3, cnt={})'.format(i))
+        plt.show()
+
+if __name__ == '__main__':
+    main()
+
+
