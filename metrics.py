804f1841392bad6f7415800ef1cc3a740198f5d5
someone <someone@example.com>
2018-09-24 23:03:32 +0900

    Replace relu(error) by elu(error)+1

--- metrics.py
+++ metrics.py
@@ -176,7 +176,7 @@ def lovasz_hinge_flat(logits, labels, weights=None):
         errors_sorted, perm = tf.nn.top_k(errors, k=tf.shape(errors)[0], name="descending_sort")
         gt_sorted = tf.gather(labelsf, perm)
         grad = lovasz_grad(gt_sorted)
-        loss = tf.tensordot(tf.nn.relu(errors_sorted), tf.stop_gradient(grad), 1, name="loss_non_void")
+        loss = tf.tensordot(tf.nn.elu(errors_sorted)+1.0, tf.stop_gradient(grad), 1, name="loss_non_void")
         return loss
 
     # deal with the void prediction case (only void pixels)
@@ -202,8 +202,8 @@ def flatten_binary_scores(scores, labels, weights=None):
     valid = tf.not_equal(weights, 0.0)
     vscores = tf.boolean_mask(scores, valid, name='valid_scores')
     vlabels = tf.boolean_mask(labels, valid, name='valid_labels')
-    weights = tf.boolean_mask(weights, valid, name='valid_weights')
-    return vscores, vlabels, weights
+    vweights = tf.boolean_mask(weights, valid, name='valid_weights')
+    return vscores, vlabels, vweights
 
 
 def mean_score_per_image(y_true, y_pred, threshold=None):
